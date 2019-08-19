4db78a356cb5de06fb59edd0e1f94a195203a7f2
Dominic Torruellas <dom@domtorr.com>
2019-08-18 19:24:52 -0700

    modified import

--- routes/projects/projectsRouter.js
+++ routes/projects/projectsRouter.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-const db = require('../../data/models/projectsModel');
+const { getProjects } = require('../../data/models/projectsModel');
 
 router.get('/', async (req, res) => {
   try {
-    const projects = await db.getProjects();
+    const projects = await getProjects();
     res.status(200).json(projects);
   } catch (err) {
     res.status(500).json({ message: 'Cannot get projects' });
