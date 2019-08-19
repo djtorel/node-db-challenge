6f56ef69a58b5c0f1ff86473789dbb6a28359d50
Dominic Torruellas <dom@domtorr.com>
2019-08-18 18:26:30 -0700

    GET - /api/projects - Get all projects

--- routes/projects/projectsRouter.js
+++ routes/projects/projectsRouter.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
+const db = require('../../data/models/projectsModel');
+
 router.get('/', async (req, res) => {
-  res.status(200).json({ message: 'projectsRouter working' });
+  try {
+    const projects = await db.getProjects();
+    res.status(200).json(projects);
+  } catch (err) {
+    res.status(500).json({ message: 'Cannot get projects' });
+  }
 });
 
 module.exports = router;
