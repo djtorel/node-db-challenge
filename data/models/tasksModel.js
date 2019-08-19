69c99b9a4b5d8656543846376123e64dae8e176c
Dominic Torruellas <dom@domtorr.com>
2019-08-18 20:02:37 -0700

    getTasksByProjectId method

--- data/models/tasksModel.js
+++ data/models/tasksModel.js
@@ -1,5 +1,7 @@
 const db = require('../dbConfig');
 
+const mapCompleted = require('../utils/mapCompleted');
+
 const getTasks = async () =>
   db('tasks as t')
     .select(
@@ -11,8 +13,16 @@ const getTasks = async () =>
       't.completed'
     )
     .join('projects as p', 'p.id', 't.project_id')
-    .orderBy('p.id', 'asc');
+    .orderBy('p.id', 'asc')
+    .map(mapCompleted);
+
+getTasksByProjectId = project_id =>
+  db('tasks as t')
+    .select('t.description', 't.notes', 't.completed')
+    .where({ project_id })
+    .map(mapCompleted);
 
 module.exports = {
   getTasks,
+  getTasksByProjectId,
 };
