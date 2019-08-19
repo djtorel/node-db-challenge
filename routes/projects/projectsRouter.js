9f2d59238d38d0f283106fd690cb9a0d98082b07
Dominic Torruellas <dom@domtorr.com>
2019-08-18 20:02:59 -0700

    GET - /api/projects/:id/tasks - Get tasks by project Id

--- routes/projects/projectsRouter.js
+++ routes/projects/projectsRouter.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const { getProjects } = require('../../data/models/projectsModel');
+const { getTasksByProjectId } = require('../../data/models/tasksModel');
 
 router.get('/', async (req, res) => {
   try {
@@ -8,6 +9,18 @@ router.get('/', async (req, res) => {
     res.status(200).json(projects);
   } catch (err) {
     res.status(500).json({ message: 'Cannot get projects' });
+  }
+});
+
+router.get('/:id/tasks', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const tasks = await getTasksByProjectId(id);
+
+    res.status(200).json(tasks);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Could not retrieve tasks for project' });
   }
 });
 
