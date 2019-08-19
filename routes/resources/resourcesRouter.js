eb99e22403fcfbf84b83ef436925b66d38917ee7
Dominic Torruellas <dom@domtorr.com>
2019-08-18 19:27:27 -0700

    GET - /api/resources - Get all resources

--- routes/resources/resourcesRouter.js
+++ routes/resources/resourcesRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 
+const { getResources } = require('../../data/models/resourcesModel');
+
 router.get('/', async (req, res) => {
-  res.status(200).json({ message: 'resourcesRouter working' });
+  const resources = await getResources();
+  res.status(200).json(resources);
 });
 
 module.exports = router;
