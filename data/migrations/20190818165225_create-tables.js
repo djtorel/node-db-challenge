8291160dd91f21ce6710e3228ac7e2ce1685b406
Dominic Torruellas <dom@domtorr.com>
2019-08-18 17:06:50 -0700

    projects migration schema

---
+++ data/migrations/20190818165225_create-tables.js
@@ -0,0 +1,55 @@
+exports.up = function(knex) {
+  return knex.schema
+    .createTable('projects', tbl => {
+      tbl.increments();
+      tbl.string('name', 128).notNullable();
+      tbl.string('description');
+      tbl
+        .boolean('completed')
+        .notNullable()
+        .defaultTo(false);
+    })
+    .createTable('resources', tbl => {
+      tbl.increments();
+      tbl.string('name', 128).notNullable();
+      tbl.string('description');
+    })
+    .createTable('taks', tbl => {
+      tbl.increments();
+      tbl.string('description').notNullable();
+      tbl.string('notes');
+      tbl
+        .boolean('completed')
+        .notNullable()
+        .defaultTo(false);
+      tbl
+        .integer('project_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    })
+    .createTable('project_resource', tbl => {
+      tbl.increments();
+      tbl
+        .integer('project_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('projects')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      tbl
+        .integer('resource_id')
+        .notNullable()
+        .unsigned()
+        .references('id')
+        .inTable('resources')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
+};
+
+exports.down = function(knex) {};
